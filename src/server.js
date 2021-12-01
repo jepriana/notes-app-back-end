2ad97239897029764667f83cb7b630e6ed708733
Wayan Jepriana <hello@jepriana.com>
2021-12-01 15:50:03 +0800

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
+// Import dotenv and run the configuration
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
+
+// Notes
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
-require('dotenv').config();
+
+// Users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const noteService = new NotesService();
+  const userService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -17,13 +27,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: noteService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: noteService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: userService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
